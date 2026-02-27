b2e0b25747406b55d8eb7d2431eb600fb516b237
EncryptEx <41539618+EncryptEx@users.noreply.github.com>
2026-02-27 10:59:47 +0100

    tasca #5 finalitzada

--- MastodonClient/src/main/java/standalone/Tasca_5.java
+++ MastodonClient/src/main/java/standalone/Tasca_5.java
@@ -1,8 +1,41 @@
 package standalone;
 
+import org.apache.hc.client5.http.fluent.Request;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ResourceBundle;
+
 public class Tasca_5 {
 
     public static void main(String[] args) {
-        System.out.println("Encara no faig res!");
-    } //TODO TasK#5
+
+    // https://mastodont.cat/api/v1/accounts/109862447110628983/statuses?limit=1
+        String URI = "https://mastodont.cat/api/v1/accounts/109862447110628983/statuses?limit=1";
+        String TOKEN = ResourceBundle.getBundle("token").getString("token");
+
+        try {
+            String output = Request.get(URI)
+                    .addHeader("Authorization","Bearer "+TOKEN)
+                    .execute()
+                    .returnContent()
+                    .asString();
+
+            var result = new JSONArray(output);
+            JSONObject firsttweet = result.getJSONObject(0);
+            String idfirsttweet = firsttweet.getString("id");
+            //System.out.println(idfirsttweet);
+
+            URI = "https://mastodont.cat/api/v1/statuses/" + idfirsttweet + "/reblog";
+            output = Request.post(URI)
+                    .addHeader("Authorization","Bearer "+TOKEN)
+                    .execute()
+                    .returnContent()
+                    .asString();
+            System.out.println(output);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
